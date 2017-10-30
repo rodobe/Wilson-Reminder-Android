0503762a348f452db3c2ff49ac14e64be9b83f9e
rodobe <31164641+rodobe@users.noreply.github.com>
2017-10-30 00:13:35 -0400

    Add files via upload

---
+++ java/com/example/jesus/wilsonreminder/SaludoWilson.java
@@ -0,0 +1,13 @@
+package com.example.jesus.wilsonreminder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SaludoWilson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saludo_wilson);
+    }
+}
